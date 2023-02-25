140db0445f88888f52585a6b7507aa6d4dc4b645
Patrycja1994 <103528688+Patrycja1994@users.noreply.github.com>
2023-02-25 19:18:48 +0000

    Deploying to gh-pages from @ Patrycja1994/goit-react-hw-08-phonebook@07b43241ff289078dec3f3cceee1cbefde605645 🚀

--- static/js/266.7712db5e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{1266:function(e,n,o){o.r(n),o.d(n,{default:function(){return i}});var r=o(184);function i(){return(0,r.jsx)("div",{children:(0,r.jsxs)("h1",{children:["Phonebook welcome page"," ",(0,r.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
-//# sourceMappingURL=266.7712db5e.chunk.js.map
